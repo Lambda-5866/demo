0597a4f1c5c5ced2d6c529115bb3d39e9e8b4584
Lambda-5866 <sksskek1gh@gmail.com>
2023-09-09 12:25:41 +0900

    "jenkins"

--- push.sh
+++ push.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 msg=$1
 
+git remote add origin https://github.com/Lambda-5866/demo.git
 git add .
 git commit -m \"$msg\"
 git branch -M main
-git push -u origin main
+git push https://Lambda-5866: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@ https://github.com/Lambda-5866/demo.git
