97b58d27ff7c025f5fbc81adae79f094f8b2e4f7
Lambda-5866 <sksskek1gh@gmail.com>
2023-09-13 11:34:16 +0900

    "test2"

--- push.sh
+++ push.sh
@@ -5,4 +5,4 @@ git remote add origin https://github.com/Lambda-5866/demo.git
 git add .
 git commit -m \"$msg\"
 git branch -M main
-git push https://Lambda-5866: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@github.com/Lambda-5866/demo.git
+git push https://Lambda-5866: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@github.com/Lambda-5866/demo.git
