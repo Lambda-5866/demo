cea30391c6421bff5602f2706b3929dd069c4a36
Lambda-5866 <sksskek1gh@gmail.com>
2023-09-13 14:26:52 +0900

    "ww"

--- push.sh
+++ push.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 msg=$1
-t=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
 
 git remote add origin https://github.com/Lambda-5866/demo.git
 git add .
 git commit -m \"$msg\"
 git branch -M main
-git push https://Lambda-5866:$t@github.com/Lambda-5866/demo.git
+git push https://github.com/Lambda-5866/demo.git
