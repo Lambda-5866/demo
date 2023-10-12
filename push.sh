402dcbeecb0c7798bb9cc8bc9ec501bf40242727
Lambda-5866 <sksskek1gh@gmail.com>
2023-10-12 04:00:26 +0000

    commit test

--- push.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-msg=$1
-git remote add origin https://github.com/Lambda-5866/demo.git
-git add .
-git commit -m \"$msg\"
-git branch -M main
-git push https://Lambda-5866@github.com/Lambda-5866/demo.git
--- test.py
+++
@@ -1,5 +0,0 @@
-print ("Hello, Python~!")
-a=3
-b=5
-
-print(a+b)
